537879a4940020102651ac4509531c72fc4e4834
Coder219 <gbdudenyc@gmail.com>
2024-08-22 17:28:09 -0400

    Cleaned up the code

--- script.js
+++ script.js
@@ -1,14 +1,20 @@
-const rpsList = ['rock','paper','scissors'];
+const choices = ['rock','paper','scissors'];
+const rounds = 5;
+const gameLogic = {
+    rock : {paper : 'computer', rock : 'tie' , scissors : 'human'},
+    paper : {paper : 'tie', rock : 'human' , scissors : 'computer'},
+    scissors : {paper : 'human', rock : 'computer' , scissors : 'tie'}
+};
 let humanScore = 0;
 let computerScore = 0;
 let ties = 0;
 function getHumanChoice(){
-    let humanChoice = prompt('Rock, paper, or scissors.');
-    while (!rpsList.includes(humanChoice)) humanChoice = prompt('Not a valid responce. Please choose rock, paper, or scissors.');
+    let humanChoice = prompt('Rock, paper, or scissors.').toLowerCase();
+    while (!choices.includes(humanChoice)) humanChoice = prompt('Not a valid responce. Please choose rock, paper, or scissors.').toLowerCase();
     return humanChoice;
 }
 function getComputerChoice(){
-    return rpsList[Math.floor(Math.random() * 3)];
+    return choices[Math.floor(Math.random() * 3)];
 }
 function printScore(){
     console.log('Computer Score: '+computerScore);
@@ -16,36 +22,28 @@ function printScore(){
     console.log('Ties: '+ ties);
     console.log('=====================')
 }
-function computerWins(){
-    computerScore += 1;
-    console.log('Computer wins!');
-}
-function humanWins(){
-    humanScore += 1;
-    console.log('Human wins!');
-}
-function tie(){
-    ties += 1;
-    console.log('Tie!');
-}
 function playRound(){
     let humanChoice = getHumanChoice();
     let computerChoice = getComputerChoice();
-    if (humanChoice == computerChoice)  tie();
-    if (humanChoice == 'rock'){
-        if (computerChoice == 'paper') computerWins();
-        if (computerChoice == 'scissors') humanWins();
-    } else if (humanChoice == 'paper'){
-        if (computerChoice == 'scissors') computerWins();
-        if (computerChoice == 'rock' ) humanWins();
-    } else if (humanChoice == 'scissors'){
-        if (computerChoice == 'rock') computerWins();
-        if (computerChoice == 'paper' ) humanWins();
+    let outcome = gameLogic[humanChoice][computerChoice];
+    switch(outcome){
+        case 'human':
+            console.log('Human Wins!');
+            humanScore ++;
+            break;
+        case 'computer':
+            console.log('Computer Wins!');
+            computerScore ++;
+            break;
+        case 'tie':
+            console.log('Tie!');
+            ties++;
+            break;
     }
     printScore();
 }
 function playGame(){
-    for (let round = 1; round <= 5; round++){
+    for (let round = 1; round <= rounds; round++){
         playRound();
     }
 }
